df8ea557e31ff56ea90b0577bf1ceaca5f0c28de
mikey Strider <dromen2001@gmail.com>
2021-07-27 23:16:37 -0700

    beginning to add styling components

---
+++ src/styling/styled.js
@@ -0,0 +1,24 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const PersonList = styled.div`
+display: flex;
+justify-content: center;
+`
+
+export const ChatBoxPage = styled.div`
+display: flex;
+justify-content: center;
+`
+
+export const PersonFormPage = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+export const GlobalStyle = createGlobalStyle`
+body {
+  height: 1200px;
+  width: 1200px;
+  margin: auto;
+}
+`
