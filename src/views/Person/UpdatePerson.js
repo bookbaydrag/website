27372ca47a18a5778e455d43b6ec7a6a599bc880
mikey Strider <dromen2001@gmail.com>
2021-07-28 22:45:12 -0700

    changed @reach to @react

--- src/views/Person/UpdatePerson.js
+++ src/views/Person/UpdatePerson.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { navigate } from '@reach/router';
+import { navigate } from '@react/router';
 
 import PersonForm from '../../components/PersonForm';
 import DeleteButton from '../../components/DeleteButton';
