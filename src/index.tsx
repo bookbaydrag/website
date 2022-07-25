07166ef3608b978becd01f08e1f6e1f74665c290
Seanny Phoenix <seannyphoenix@gmail.com>
2022-07-24 17:18:11 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,4 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 createRoot(document.getElementById('root')!)
-    .render(<StrictMode><App> /></StrictMode>);
+    .render(<StrictMode><App /></StrictMode>);
